6d0f6145e9139940fad454accd425792491b915b
ivancas84 <ivancas84@gmail.com>
2020-07-24 00:34:43 -0300

    Desarrollo de sistema-fieldset

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DataDefinitionService } from '@service/data-definition/data-definition.service';
+import { DataDefinitionLoaderService } from '@service/data-definition-loader.service';
+import { SessionStorageService } from '@service/storage/session-storage.service';
+import { ParserService } from '@service/parser/parser.service';
+import { ValidatorsService } from '@service/validators/validators.service';
+import { HttpClientModule } from '@angular/common/http';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { MenuComponent } from './component/menu/menu.component'
 
-import { SistemaAdminComponent } from './component/sistema-admin/sistema-admin/sistema-admin.component'
+import { SistemaAdminComponent } from './component/sistema-admin/sistema-admin/sistema-admin.component';
+import { SistemaFieldsetComponent } from './component/sistema-admin/sistema-fieldset/sistema-fieldset.component'
+import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
+
 
 
 @NgModule({
@@ -23,19 +35,32 @@ import { SistemaAdminComponent } from './component/sistema-admin/sistema-admin/s
     MenuComponent,
 
     SistemaAdminComponent,
+    SistemaFieldsetComponent,
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
 
-    MatToolbarModule,
-    MatMenuModule,
     MatButtonModule,
+    MatCardModule,
     MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatInputModule,
+    
   ],
-  providers: [],
+  providers: [
+    DataDefinitionService, 
+    DataDefinitionLoaderService, 
+    SessionStorageService, 
+    ParserService, 
+    ValidatorsService,
+    {provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/component/sistema-admin/sistema-fieldset/sistema-fieldset.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { FieldsetComponent } from '../../../core/component/fieldset/fieldset.component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { DataDefinitionService } from '../../../core/service/data-definition/data-definition.service';
+import { ValidatorsService } from '../../../core/service/validators/validators.service';
+import { SessionStorageService } from '../../../core/service/storage/session-storage.service';
+
+@Component({
+  selector: 'app-sistema-fieldset',
+  templateUrl: './sistema-fieldset.component.html',
+})
+export class SistemaFieldsetComponent extends FieldsetComponent {
+
+  readonly entityName: string = 'sistema';
+
+  constructor(
+    protected fb: FormBuilder, 
+    protected dd: DataDefinitionService, 
+    protected validators: ValidatorsService,
+    protected router: Router, 
+    protected storage: SessionStorageService 
+  ) {
+    super(router, storage);
+  }
+
+  formGroup(): FormGroup {
+    let fg: FormGroup = this.fb.group({
+      id:null,
+      nombre: [null, {
+        validators: [Validators.required],
+      }],
+    });
+    return fg;
+  }
+
+  get id() { return this.fieldset.get('id')}
+  get nombre() { return this.fieldset.get('nombre')}
+  get formacion() { return this.fieldset.get('formacion')}
+  get clasificacion() { return this.fieldset.get('clasificacion')}
+  get codigo() { return this.fieldset.get('codigo')}
+  get perfil() { return this.fieldset.get('perfil')}
+
+}
