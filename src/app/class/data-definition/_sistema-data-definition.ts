7415fef5eeba33e7f1ca39f69f03fc644db7b2bb
ivancas84 <ivancas84@gmail.com>
2020-07-28 08:55:13 -0300

    Actualizar label

--- src/app/class/data-definition/_sistema-data-definition.ts
+++ src/app/class/data-definition/_sistema-data-definition.ts
@@ -12,4 +12,13 @@ export class _SistemaDataDefinition extends DataDefinition {
     this.stg.setItem("sistema" + rowCloned.id, rowCloned);
   }
 
+  label (id: string | number): string {
+    var row = this.stg.getItem(this.entity + id);
+    if(!row) return null;
+
+    let ret = "";
+    if (row["nombre"]) ret = ret.trim() + " " + row["nombre"];
+
+    return ret.trim();
+  }
 }
